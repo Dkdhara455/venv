f9c7edb43f4757364b94f3fd93bf78eacab5a88f
Deepak <dharadeepakkumar2000@gmail.com>
2025-08-05 11:47:41 +0530

    lting.py update

--- lting.py
+++ lting.py
@@ -25,3 +25,10 @@ main()
 
 # ruff check lting.py-->it show your code error in terminal(gives linting suggestions)
 # for correct/auto-format:black <your_file>.py
+# run this program to show those files which path you declared .
+#Enter a directory path: C:\Users\ASUS\OneDrive\Desktop\Aimaven internship\my_project
+# Files:
+# lting.py
+# pythonvenv.txt
+# python_linting.txt
+# requirements.txt
